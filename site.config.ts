94654deec28e55a933531992cafcef32c50596ad
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-06-03 19:50:30 +0700

    chore(site.config): :wrench: adjust config based on my preferences

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '43ddd8a385ae4625b827d1c71b207c90',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'College Space',
+  domain: 'college.yehezgun.com',
+  author: 'Yehezkiel Gunawan',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description:
+    'A site integrated with my own Notion Space to put my master college notes. Made using Nextjs-notion-starter-kit by Travis Fischer',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'YehezGun',
+  github: 'yehezkielgunawan',
+  linkedin: 'yehezkiel-gunawan-595128138',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
